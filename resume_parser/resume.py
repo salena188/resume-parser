bdeba25481f6827ac8ac0cbb6ae399d9b20d7792
salena188 <salinashrestha188@gmail.com>
2026-03-10 08:21:04 +0545

    resume

---
+++ resume_parser/resume.py
@@ -0,0 +1,24 @@
+logo = "logo.png"   # image logo file
+
+name = input("Enter your name: ")
+headline = input("Enter your headline: ")
+bio = input("Enter your bio: ")
+resume_link = input("Enter your resume link: ")
+email = input("Enter your email: ")
+phone = input("Enter your phone number: ")
+
+print("\n----- PERSONAL RESUME -----")
+
+print("Logo:", logo)
+print("Name:", name)
+print("Headline:", headline)
+
+print("\nBio:")
+print(bio)
+
+print("\nResume Link:")
+print(resume_link)
+
+print("\nConnect with me:")
+print("Email:", email)
+print("Phone:", phone)
