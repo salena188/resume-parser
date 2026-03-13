8842c7ec7504443d89926ced68bff446e02b1aff
salena188 <salinashrestha188@gmail.com>
2026-03-13 07:04:06 +0545

    feature 4

---
+++ resume-project-counter.py
@@ -0,0 +1,7 @@
+#check number of project
+
+projects = int(input("How many projects are in your resume? "))
+
+for i in range(projects):
+   project = input("Enter project name: ")
+   print("Project added:", project)
