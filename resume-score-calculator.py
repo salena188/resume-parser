d0e8a6327022869b69410bf7a0acd936de999519
salena188 <salinashrestha188@gmail.com>
2026-03-13 07:05:49 +0545

    feature5

---
+++ resume-score-calculator.py
@@ -0,0 +1,12 @@
+score = 0
+
+python = input("Do you know Python? (yes/no): ")
+aws = input("Do you know AWS? (yes/no): ")
+
+if python == "yes":
+   score = score + 10
+
+if aws == "yes":
+   score = score + 10
+
+print("Resume Score:", score)
